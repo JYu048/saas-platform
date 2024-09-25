4d5528861e117a2921da60727f314646dab69e96
Jerry Yu <JaredY3325@Gmail.com>
2024-09-25 00:23:21 -0600

    Configured Serilog

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -1,9 +1,17 @@
 using AuctionService.Data;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// Serilog Configuration
+Log.Logger = new LoggerConfiguration()
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .WriteTo.File("logs/log-.txt", rollingInterval: RollingInterval.Month) // Log to a file, new file per month
+    .CreateLogger();
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AuctionDbContext>(options =>
@@ -38,11 +46,18 @@ app.MapControllers();
 
 try
 {
+    Log.Information("Starting database initialization...");
     DbInitializer.InitDB(app);
+    Log.Information("Database initialization completed successfully.");
 }
 catch (Exception e)
 {
-    Console.WriteLine(e);
+    Log.Fatal(e, "An error occurred while seeding the database.");
+    return;
+}
+finally
+{
+    Log.CloseAndFlush(); // Flush all logs before app closes
 }
 
 app.UseAuthorization();
